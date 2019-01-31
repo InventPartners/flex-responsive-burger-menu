ef029c4b011130cb766e5b0d6c967cb0e325a3c2
Matthew Bradley <matt@inventpartners.com>
2019-01-31 16:53:37 +0000

    Added mask

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,21 @@
 $('document').ready(function(){
+	
 	$('body').on('click' , '#inv-nav-main-btn' , function(){
 		if($('#inv-nav-main-btn').hasClass('is-active')){
+			$('#inv-nav-mask').remove();
 			$('#inv-nav-main-btn').removeClass('is-active');
 			$('#inv-menu').removeClass('menu-show');
 		} else {
+
+			$('body').append('<div id="inv-nav-mask"></div>');
 			$('#inv-nav-main-btn').addClass('is-active');
 			$('#inv-menu').addClass('menu-show');
 		}
+	});
+	$('body').on('click' , '#inv-nav-mask' , function(){
+		$('#inv-nav-mask').remove();
+		$('#inv-nav-main-btn').removeClass('is-active');
+		$('#inv-menu').removeClass('menu-show');
 	});
 	
 	checkAffix();
